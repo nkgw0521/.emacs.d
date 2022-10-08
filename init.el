a988c7abf0ebc3f933344dd061092b3ee97798b0
nkgw0521 <0521nkgw@gmail.com>
2022-10-08 11:29:58 +0900

    2022/10/8 update

--- init.el
+++ init.el
@@ -9,8 +9,8 @@
              '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
+;(add-to-list 'package-archives
+;             '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
 
